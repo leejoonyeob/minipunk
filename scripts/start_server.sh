1d78bfcfdf591c01c29c60b52bdb804d7b57b56b
yeob_dis <168530695+leejoonyeob@users.noreply.github.com>
2025-01-01 01:39:16 +0900

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+if ! command -v docker &> /dev/null
+then
+    echo "Docker could not be found. Installing Docker..."
+    sudo yum install -y docker
+    sudo systemctl start docker
+    sudo systemctl enable docker
+fi
+
 docker load -i minipunk_image.tar
